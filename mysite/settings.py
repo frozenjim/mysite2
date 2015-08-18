40314064a52a843861b1dfa849adc278e7dee208
frozenjim <james@whitehousenorth.com>
2015-08-18 15:55:15 -0400

    suddenly nothing works.  Always with error "No module named 'django.templates'"
    I changed nothing.  It just failed.  Nothing on Google or StackExchange.
    
    Reverting to last good version to see if the error persists.

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,13 +55,15 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        #original boilerplate seems wrong(?)  'templates' should be 'template'
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        #'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.templates.context_processors.debug',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
