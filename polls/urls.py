89a9bb17fc5b0d9e4d7a35dd2cc8c877999cb61f
frozenjim <james@whitehousenorth.com>
2015-08-20 15:25:42 -0400

    Tutorial #4: Use generic views

--- polls/urls.py
+++ polls/urls.py
@@ -5,14 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    #ex: /polls/
-    url(r'^$', views.index, name='index'),
-    #ex /polls/5/
-    url(r'(?P<question_id>[0-9+])/$',           views.detail,   name='detail'),
-    #ex /polls/question5/
-    url(r'question(?P<question_id>[0-9]+)/$',           views.detail,   name='detail2'),
-    #ex /polls/5/results/
-    url(r'(?P<question_id>[0-9]+)/results/',    views.results,  name='results'),
-    #ex /polls/5/vote/
+    url(r'^$',                                  views.IndexView.as_view(),      name='index'),
+    url(r'(?P<question_id>[0-9+])/$',           views.DetailView.as_view(),     name='detail'),
+    url(r'(?P<question_id>[0-9]+)/results/',    views.ResultView.as_view(),     name='results'),
     url(r'(?P<question_id>[0-9]+)/vote/',       views.vote,     name='vote'),
 ]
